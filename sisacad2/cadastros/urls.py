d6fce897830593770fcd7d28707b22f9b1b6f27b
ulyssessousa <ulyssessousa@gmail.com>
2025-01-15 14:29:39 -0300

    Criação dos formulários de cadastro de Departamento e Disciplina

--- sisacad2/cadastros/urls.py
+++ sisacad2/cadastros/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from .views import DepartamentoCreate, DisciplinaCreate
 
 urlpatterns = [
+    path('cadastros/departamento/', 
+         DepartamentoCreate.as_view(),
+         name='cadastrar-departamento'),
     
+    path('cadastros/disciplina/',
+         DisciplinaCreate.as_view(),
+         name='cadastrar-disciplina'),
 ]
--- sisacad2/cadastros/views.py
+++ sisacad2/cadastros/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Departamento, Disciplina
+from django.urls import reverse_lazy
 
 # Create your views here.
+class DepartamentoCreate(CreateView):
+    model = Departamento
+    fields = ['nome', 'sigla']
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy('inicio')
+
+class DisciplinaCreate(CreateView):
+    model = Disciplina
+    fields = ['nome', 'cargaHoraria', 'departamento']
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy('inicio')
+
