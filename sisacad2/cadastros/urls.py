df31443070b2eb26af324a39d871a5ad7d8b4913
ulyssessousa <ulyssessousa@gmail.com>
2025-01-17 13:48:54 -0300

    Adição das funcionalidades de edição e exclusão de Departamentos e de Disciplinas

--- sisacad2/cadastros/urls.py
+++ sisacad2/cadastros/urls.py
@@ -1,12 +1,28 @@
 from django.urls import path
-from .views import DepartamentoCreate, DisciplinaCreate
+from .views import DepartamentoCreate, DisciplinaCreate, DepartamentoUpdate, DisciplinaUpdate, DepartamentoDelete, DisciplinaDelete
 
 urlpatterns = [
-    path('cadastros/departamento/', 
+     path('cadastros/departamento/', 
          DepartamentoCreate.as_view(),
          name='cadastrar-departamento'),
     
-    path('cadastros/disciplina/',
+     path('cadastros/disciplina/',
          DisciplinaCreate.as_view(),
          name='cadastrar-disciplina'),
+     
+     path('editar/departamento/<int:pk>/',
+          DepartamentoUpdate.as_view(),
+          name='editar-departamento'),
+     
+     path('editar/disciplina/<int:pk>/',
+          DisciplinaUpdate.as_view(),
+          name='editar-disciplina'),
+
+     path('excluir/departamento/<int:pk>',
+          DepartamentoDelete.as_view(),
+          name='excluir-departamento'),
+     
+     path('excluir/disciplina/<int:pk>',
+          DisciplinaDelete.as_view(),
+          name='excluir-disciplina'),
 ]
--- sisacad2/cadastros/views.py
+++ sisacad2/cadastros/views.py
@@ -1,6 +1,7 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Departamento, Disciplina
 from django.urls import reverse_lazy
+
 
 # Create your views here.
 class DepartamentoCreate(CreateView):
@@ -15,3 +16,24 @@ class DisciplinaCreate(CreateView):
     template_name = "cadastros/form.html"
     success_url = reverse_lazy('inicio')
 
+class DepartamentoUpdate(UpdateView):
+    model = Departamento
+    fields = ['nome', 'sigla']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('inicio')
+
+class DisciplinaUpdate(UpdateView):
+    model = Disciplina
+    fields = ['nome', 'cargaHoraria', 'departamento']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('inicio')
+
+class DepartamentoDelete(DeleteView):
+    model = Departamento
+    template_name = 'cadastros/form-excluir.html'
+    success_url = reverse_lazy('inicio')
+
+class DisciplinaDelete(DeleteView):
+    model = Disciplina
+    template_name = 'cadastros/form-excluir.html'
+    success_url = reverse_lazy('inicio')
