322aeb4c4d783e478a5fd2bcdba82fc618322d70
ulyssessousa <ulyssessousa@gmail.com>
2025-01-22 16:29:27 -0300

    Formatação de formuluários com crispy_forms

--- sisacad2/sisacad2/settings.py
+++ sisacad2/sisacad2/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'paginas.apps.PaginasConfig',
-    'cadastros.apps.CadastrosConfig'
+    'cadastros.apps.CadastrosConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
