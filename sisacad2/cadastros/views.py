ed7b30045d1f08c0c463f974f1114cb44498a0a7
ulyssessousa <ulyssessousa@gmail.com>
2025-02-05 15:20:35 -0300

    Acesso por grupo de usuários

--- sisacad2/cadastros/views.py
+++ sisacad2/cadastros/views.py
@@ -3,48 +3,63 @@ from .models import Departamento, Disciplina
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from braces.views import GroupRequiredMixin
 
 
 # Create your views here.
-class DepartamentoCreate(LoginRequiredMixin, CreateView):
+class DepartamentoCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador"
     model = Departamento
     fields = ['nome', 'sigla']
     template_name = "cadastros/form.html"
     success_url = reverse_lazy('listar-departamento')
     login_url = reverse_lazy('login')
 
-class DisciplinaCreate(CreateView):
+class DisciplinaCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador"
     model = Disciplina
     fields = ['nome', 'cargaHoraria', 'departamento']
     template_name = "cadastros/form.html"
     success_url = reverse_lazy('listar-disciplina')
+    login_url = reverse_lazy('login')
 
-class DepartamentoUpdate(UpdateView):
+class DepartamentoUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador"
     model = Departamento
     fields = ['nome', 'sigla']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-departamento')
+    login_url = reverse_lazy('login')
 
-class DisciplinaUpdate(UpdateView):
+
+class DisciplinaUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador"
     model = Disciplina
     fields = ['nome', 'cargaHoraria', 'departamento']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-disciplina')
-
-class DepartamentoDelete(DeleteView):
+    login_url = reverse_lazy('login')
+    
+class DepartamentoDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador"
     model = Departamento
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-departamento')
+    login_url = reverse_lazy('login')
 
-class DisciplinaDelete(DeleteView):
+class DisciplinaDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador"
     model = Disciplina
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-disciplina')
+    login_url = reverse_lazy('login')
 
-class DepartamentoList(ListView):
+class DepartamentoList(LoginRequiredMixin, ListView):
     model = Departamento
     template_name = 'cadastros/lista/departamento.html'
+    login_url = reverse_lazy('login')
 
-class DisciplinaList(ListView):
+class DisciplinaList(LoginRequiredMixin, ListView):
     model = Disciplina
     template_name = 'cadastros/lista/disciplina.html'
+    login_url = reverse_lazy('login')
