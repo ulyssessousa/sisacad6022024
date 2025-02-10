88f5474a593c07e1e775ec870e302a33b780a4d1
ulyssessousa <ulyssessousa@gmail.com>
2025-02-10 16:05:18 -0300

    Adição das funcionalidades CRUD para Modalidade e Curso

--- sisacad2/cadastros/admin.py
+++ sisacad2/cadastros/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Departamento, Disciplina
+from .models import Departamento, Disciplina, Modalidade, Curso
 
 # Register your models here.
 admin.site.register(Departamento)
 admin.site.register(Disciplina)
+admin.site.register(Modalidade)
+admin.site.register(Curso)
