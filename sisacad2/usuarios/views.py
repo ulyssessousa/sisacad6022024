0e17c3fc989698360a2ae02db264cc48e983227f
ulyssessousa <ulyssessousa@gmail.com>
2025-02-14 11:42:01 -0300

    Associação de usuário a grupo aluno no momento do cadastro

--- sisacad2/usuarios/views.py
+++ sisacad2/usuarios/views.py
@@ -6,11 +6,21 @@ from django.views.generic.edit import CreateView
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
 
+from django.contrib.auth.models import Group
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 class UsuarioCreate(CreateView):
     template_name = "cadastros/form.html"
     form_class = UsuarioForm
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        grupo = get_object_or_404(Group, name="Aluno")
+        url = super().form_valid(form)
+        self.object.groups.add(grupo)
+        self.object.save()
+        return url
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
