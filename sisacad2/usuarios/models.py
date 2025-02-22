66e5e2c8901b3665a9a5de6910ad10c1c2cc7620
ulyssessousa <ulyssessousa@gmail.com>
2025-02-22 11:58:57 -0300

    Aplicação de máscaras em campos de texto

--- sisacad2/usuarios/models.py
+++ sisacad2/usuarios/models.py
@@ -6,8 +6,8 @@ class Perfil(models.Model):
     nome_completo = models.CharField(max_length=80, 
                                     null=True, 
                                     verbose_name='Nome completo')
-    cpf = models.CharField(max_length=11, null=True,
+    cpf = models.CharField(max_length=14, null=True,
                                     verbose_name="CPF")
-    telefone = models.CharField(max_length=11, null=True)
+    telefone = models.CharField(max_length=16, null=True)
 
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
